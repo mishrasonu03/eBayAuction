951d736061b98fb5e9d4d072bfe5393e69b236a4
eschiang2 <eschiang2@gmail.com>
2016-01-28 18:19:20 -0800

    updated query

--- runLoad.sh
+++ runLoad.sh
@@ -4,18 +4,18 @@ mysql CS144 < drop.sql
 mysql CS144 < create.sql
 
 ant
-ant run
+#ant run
 #Use this to run on all data
-#ant run-all
+ant run-all
 
-sort -u items.csv > items.csv
-sort -u categories.csv > categories.csv
-sort -u bids.csv > bids.csv
-sort -u users.csv > users.csv
+#sort -u items.csv > items.csv
+#sort -u categories.csv > categories.csv
+#sort -u bids.csv > bids.csv
+#sort -u users.csv > users.csv
 
 mysql CS144 < load.sql
 
-rm items.csv
-rm categories.csv
-rm bids.csv
-rm users.csv
+rm itemTable.dat
+rm categoryTable.dat
+rm userTable.dat
+rm bidTable.dat
