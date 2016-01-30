e42db35bb13bf03c09d9ce069b7be6b544d1621b
mishrasonu1993 <mishrasonu1993@gmail.com>
2016-01-29 16:33:33 -0800

    Sorting done

--- runLoad.sh
+++ runLoad.sh
@@ -4,18 +4,22 @@ mysql CS144 < drop.sql
 mysql CS144 < create.sql
 
 ant
-ant run
+#ant run
 #Use this to run on all data
-#ant run-all
+ant run-all
 
-sort -u items.csv > items.csv
-sort -u categories.csv > categories.csv
-sort -u bids.csv > bids.csv
-sort -u users.csv > users.csv
+sort -u itemTable.dat > itemTable.dat
+sort -u categoryTable.dat > categoryTable.dat
+sort -u userTable.dat > userTable.dat
+sort -u bidTable.dat > bidTable.dat
+sort -u sellerTable.dat > sellerTable.dat
+sort -u bidderTable.dat > bidderTable.dat
 
 mysql CS144 < load.sql
 
-rm items.csv
-rm categories.csv
-rm bids.csv
-rm users.csv
+rm itemTable.dat
+rm categoryTable.dat
+rm userTable.dat
+rm bidTable.dat
+rm sellerTable.dat
+rm bidderTable.dat
